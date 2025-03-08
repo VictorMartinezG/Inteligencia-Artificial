aeec0acdd3c75d8cfa4ecfbb035ea3a3789ec29e
VictorMartinezG <143561867+VictorMartinezG@users.noreply.github.com>
2025-03-08 17:11:13 -0600

    003_Sistemas numericos,py
    Practica 3

---
+++ 003_Sistemas numericos.py
@@ -0,0 +1,8 @@
+
+#Sistemas numericos
+binario = 0b1010  # 10 en decimal
+octal = 0o12  # 10 en decimal
+hexadecimal = 0xA  # 10 en decimal
+
+print(f"Binario: {binario}, Octal: {octal}, Hexadecimal: {hexadecimal}")
+# Salida: Binario: 10, Octal: 10, Hexadecimal: 10
