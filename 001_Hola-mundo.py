2579683387108294f8230ee3050053829e99b6c6
VictorMartinezG <143561867+VictorMartinezG@users.noreply.github.com>
2025-03-08 16:49:58 -0600

    001_Hola-mundo.py
    Practica 1

--- 001_Hola-mundo.py
+++ 001_Hola-mundo.py
@@ -1,1 +1,14 @@
-print("hola mundo")
+
+# Funcion del print
+print("hola mundo") # Mortar hola mundo en la terminal 
+
+#Funciones integradas
+print(len("Python"))  # Devuelve 6, porque "Python" tiene 6 caracteres.
+print(type(3.14))  # Devuelve <class 'float'>, porque 3.14 es un número decimal.
+
+# Uso de sep y end
+print("Hola", "Mundo", "!")  # Imprime: Hola Mundo !
+print("Python", "es", "cool", sep="-")  # Imprime: Python-es-cool
+print("Este mensaje", end=" ")  # No cambia de línea
+print("sigue en la misma línea.")  # Se imprimirá en la misma línea.
+
