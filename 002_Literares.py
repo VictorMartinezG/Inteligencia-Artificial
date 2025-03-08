8b6f33237b7eadc8162e752217c0b34bbe4de1b1
VictorMartinezG <143561867+VictorMartinezG@users.noreply.github.com>
2025-03-08 16:59:34 -0600

    002_Literares.py
    Practica 2

---
+++ 002_Literares.py
@@ -0,0 +1,11 @@
+# Literales numéricos
+numero = 123
+pi = 3.1416
+
+# Literal de cadena
+mensaje = "Hola, soy un literal de cadena."
+
+# Imprimir valores
+print(numero)  # 123
+print(pi)  # 3.1416
+print(mensaje)  # Hola, soy un literal de cadena.
