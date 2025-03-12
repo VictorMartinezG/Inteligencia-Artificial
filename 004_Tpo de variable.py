208cbc7eb508e4c5bbc900750da68c1eb893b369
VictorMartinezG <143561867+VictorMartinezG@users.noreply.github.com>
2025-03-11 22:00:02 -0600

    004_Tipo de variable.py
    Practica 4

---
+++ 004_Tpo de variable.py
@@ -0,0 +1,23 @@
+
+#Entero
+a = 10
+b = 5
+suma = a + b
+
+print(f"La suma de {a} y {b} es {suma}.")  
+# Salida: La suma de 10 y 5 es 15.
+
+#Flotante
+radio = 2.5
+pi = 3.1416
+area = pi * (radio ** 2)
+
+print(f"El área de un círculo con radio {radio} es {area:.2f}.")  
+# Salida: El área de un círculo con radio 2.5 es 19.63.
+
+#caracter
+cadena1 = 'I\'m learning Python!'
+cadena2 = "El dijo: \"Python es genial!\""
+
+print(cadena1)  # I'm learning Python!
+print(cadena2)  # El dijo: "Python es genial!"
