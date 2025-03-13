dd3e6f2cc8ab49ac62c3b7d8b47442ec38a16c12
VictorMartinezG <143561867+VictorMartinezG@users.noreply.github.com>
2025-03-13 15:22:49 -0600

    009_Operador binario.py
    Practica 9

---
+++ 009_Operador binario.py
@@ -0,0 +1,9 @@
+# Número de productos en dos paquetes diferentes
+paquete1 = 8
+paquete2 = 5
+
+# Operación binaria de suma para calcular el total de productos
+total_productos = paquete1 + paquete2
+
+print(f"Total de productos en ambos paquetes: {total_productos}")
+# Salida: Total de productos en ambos paquetes: 13
