cf1cd7c1169d74a196ba1ee132d2fa706823d660
VictorMartinezG <143561867+VictorMartinezG@users.noreply.github.com>
2025-03-13 15:12:06 -0600

    007_Operadores.py
    Practica 7

---
+++ 007_Operadores.py
@@ -0,0 +1,11 @@
+# Programa que realiza operaciones básicas entre dos números
+num1 = 12
+num2 = 4
+
+suma = num1 + num2  # Suma
+resta = num1 - num2  # Resta
+multiplicacion = num1 * num2  # Multiplicación
+division = num1 / num2  # División clásica
+
+print(f"Suma: {suma}, Resta: {resta}, Multiplicación: {multiplicacion}, División: {division}")
+# Salida: Suma: 16, Resta: 8, Multiplicación: 48, División: 3.0
