dce2b7895a8f2700e50bc2e9a25639e9b5152874
VictorMartinezG <143561867+VictorMartinezG@users.noreply.github.com>
2025-03-13 15:47:54 -0600

    015_Sensibilidad a mayusculas.py
    Practica 15

---
+++ 015_Sensibilidad a mayusculas.py
@@ -0,0 +1,10 @@
+
+# Definiendo credenciales de acceso
+Usuario = "Admin123"
+usuario = "admin123"
+
+# Comparación entre las variables (el sistema distingue mayúsculas y minúsculas)
+if Usuario == usuario:
+    print("Acceso concedido.")
+else:
+    print("Acceso denegado.")
