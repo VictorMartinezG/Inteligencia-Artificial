581ab56b3dea2abf81c8d96413e6fe8ef90f41ce
VictorMartinezG <143561867+VictorMartinezG@users.noreply.github.com>
2025-03-13 15:31:55 -0600

    013_Exponencial.py
    Practica 13

---
+++ 013_Exponencial.py
@@ -0,0 +1,6 @@
+
+# Exponenciación encadenada
+resultado = 2 ** 3 ** 2  # Se evalúa como 2 ** (3 ** 2) = 2 ** 9 = 512
+
+print(f"Resultado de 2 ** 3 ** 2: {resultado}")
+# Salida: Resultado de 2 ** 3 ** 2: 512
