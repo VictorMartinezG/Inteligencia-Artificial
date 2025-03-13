62ee8c27f7d723814e05da19ff634c0351ea836c
VictorMartinezG <143561867+VictorMartinezG@users.noreply.github.com>
2025-03-13 15:19:47 -0600

    010_Operador unitario.py
    Practica 10

---
+++ 010_Operador unitario.py
@@ -0,0 +1,8 @@
+saldo = 500  # Saldo positivo
+deuda = -200  # Representamos la deuda con un signo negativo
+
+# Uso de operadores unarios
+nuevo_saldo = saldo + deuda  # Aquí sumamos saldo y deuda (como número negativo)
+
+print(f"Saldo después de pagar la deuda: {nuevo_saldo}")
+# Salida: Saldo después de pagar la deuda: 300
