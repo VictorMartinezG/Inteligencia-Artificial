f4f1a95efbb61c46d4b132469ab0db200d0f87b0
VictorMartinezG <143561867+VictorMartinezG@users.noreply.github.com>
2025-03-13 15:09:01 -0600

    006_Expresiones en Py.py
    Practica 6

---
+++ 006_Expresiones en Py.py
@@ -0,0 +1,8 @@
+
+# Cálculo del área de un rectángulo usando una expresión
+base = 10
+altura = 5
+area = base * altura  # Expresión: Multiplica base por altura
+
+print(f"El área del rectángulo es: {area}")
+# Salida: El área del rectángulo es: 50
