5788c5f1edb286f77d950d780c6c6e41f1a285b3
VictorMartinezG <143561867+VictorMartinezG@users.noreply.github.com>
2025-03-16 09:57:18 -0600

    027_Lista.py
    Practica 27

---
+++ 027_Lista.py
@@ -0,0 +1,9 @@
+
+
+# -------------------------------------------------
+# Definiendo una lista con diferentes tipos de datos
+# -------------------------------------------------
+my_list = [1, None, True, "Soy una cadena", 256, 0]
+
+# Mostrando la lista completa
+print("Lista original:", my_list)
